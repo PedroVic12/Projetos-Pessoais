12f59193e19087c14a3219bafe92ee54892d74a0
PedroVic12 <pedrovictor.rveras12@gmail.com>
2023-01-16 13:00:12 -0300

    .

--- Flutter/start_the_day/lib/main.dart
+++ Flutter/start_the_day/lib/main.dart
@@ -1,6 +1,12 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 //import 'package:audioplayers/audioplayers.dart';
+//import 'package:url_launcher/url_launcher.dart';
+
+
+//- Barra de Rolagem
+// - 3 Telas de Navegação para cada arquivo
+// - Flutter for Dummies
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +22,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
-  String texto = '';
+
+  String ObjetivoTexto = '';
+
+  List<String> superDesenvolvedor = [
+    "Habilidades de Resultados Rapidos",
+    "Habilidades de Acelereção de Carreira",
+    "Habilidades de Marketing Pessoal",
+    "Domine uma linguagem e um framework, CRUD, Apis, Filtros, Relatorios, Banco de Dados"
+  ];
+
+  List<String> filmes = ["Ouse Sonhar Netflix", "Lobo de Wall Street"];
+
+  List<String> hormonios = ["Dopamine", "Oxytocin", "Endorphins", "Serotonin"];
+
+  List<String> objetivos = [
+    "Chatbot Whatsapp",
+    "Flutter Todo List + Organização + Flutter For Dummies + Assistente com Voz",
+    "Muscle Up"
+  ];
+
+
   List<String> lista = [
     "Arrumar o Portifolio (Gohan Treinamentos)",
     "Fazer 1 Projeto Flutter",
@@ -29,53 +54,88 @@ class _MyAppState extends State<MyApp> {
     "Qual a inspiração do dia de hoje? Medite, busque a serenidade e encontre a resposta",
     "Qual a mensagem pro meu coração e como pode ser util para todos os seres humanos e para todos os seres do planeta Terra? (Pense no bem maior)"
   ];
+void showHormonios(){
+  setState(() {
+    ObjetivoTexto = hormonios[Random().nextInt(hormonios.length)];
+  });
+}
+
 
   void showText() {
     setState(() {
-      texto = lista[Random().nextInt(lista.length)];
+      ObjetivoTexto = objetivos[Random().nextInt(objetivos.length)];
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("Voce merece esse APP")
-        ),
+        appBar: AppBar(title: const Text("Voce merece esse APP")),
         body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  child: Text("Link para o Google"),
+                  onTap: () {
+                    //launch('https://www.google.com/');
+                    print("1:35 The Batman!");
+                  },
+                ),
 
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:[
-
-                  Text(texto,style: const TextStyle(color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40)),
-
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue, // background
-                      onPrimary: Colors.white, // foreground
-                    ),
-                    onPressed: showText,
-                    child: const Text('Gerar Tarefa Aleatória',
-                        style: TextStyle(fontSize: 30)),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 5)
                   ),
 
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.red, width: 5)
-                    ),
-                    child: const Text("==> TODO LIST \n\n - Objetivos do dia \n\n -Projetos \n\n - Horas de Pomodoro \n\n https://docs.flutter.dev/cookbook",
-                      style: TextStyle(fontSize: 30),
-                    ), )
-                ]
-            ),
-          ),
-          ),
-        );
+                  child: Column(
+                    children: [
 
+                      const Text(
+                        "Objetivos do dia",
+                        style: TextStyle(
+                            fontFamily: AutofillHints.jobTitle, fontSize: 35),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: Text(ObjetivoTexto,
+                            style: const TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25)),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue, // background
+                            onPrimary: Colors.white, // foreground
+                          ),
+                          onPressed: showText,
+                          child: const Text('Gerar Tarefa Aleatória',
+                              style: TextStyle(fontSize: 30)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 5)),
+                  child: const Text(
+                    "==> TODO LIST \n\n - Objetivos do dia \n\n -Projetos \n\n - Horas de Pomodoro \n\n https://docs.flutter.dev/cookbook",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                )
+              ]),
+        ),
+      ),
+    );
   }
 }
