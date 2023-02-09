b4475967aa1a361f78c8d054c5fccd0f0fd75fd7
PedroVic12 <pedrovictor.rveras12@gmail.com>
2023-02-09 10:56:05 -0300

    .

---
+++ Flutter/start_the_day/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/pedrov/developer-flutter/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/pedrov/Documentos/GitHub/Projetos-Pessoais/Flutter/start_the_day" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/pedrov/developer-flutter/flutter"
+  "PROJECT_DIR=/home/pedrov/Documentos/GitHub/Projetos-Pessoais/Flutter/start_the_day"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/pedrov/Documentos/GitHub/Projetos-Pessoais/Flutter/start_the_day/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/pedrov/Documentos/GitHub/Projetos-Pessoais/Flutter/start_the_day/lib/main.dart"
+)
